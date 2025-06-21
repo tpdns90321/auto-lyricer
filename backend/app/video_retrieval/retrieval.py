3e4941c8db9e268f312627f47e7ca824e1e082b7
tpdns90321 <tpdns9032100@gmail.com>
2025-06-22 01:42:43 +0900

    Update retrieval dependencies, extract subtitle
    1. Add depednecies for missing dev and extracting subtitle from YouTube.
    2. Add method for fetching subtitle from video platform.

--- backend/app/video_retrieval/retrieval.py
+++ backend/app/video_retrieval/retrieval.py
@@ -1,11 +1,16 @@
-from ..shared.data import Audio, AudioExtension
+from ..shared.data import Audio, AudioExtension, SubtitleExtension, Transcription
+from ..shared.supported import Language
+from ..shared.exception import UnsupportedPlatformError
 from .monkey_patch import gevent
 from .type import VideoInfo
 from .exception import VideoExtractError
 
 import yt_dlp
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import WebVTTFormatter
 import asyncio
 import asyncio_gevent
+from urllib.parse import urlparse
 
 
 class VideoRetrieval:
@@ -61,3 +66,47 @@ class VideoRetrieval:
             binary=binary,
             extension=AudioExtension.AAC,
         )
+
+    async def retrieval_subtitle_of_video(
+        self, url: str, target_language: Language | None = None
+    ) -> Transcription:
+        parsed_url = urlparse(url)
+        hostname: str = parsed_url.hostname or ""
+        video_id = ""
+        if (
+            "youtube.com" in hostname
+            and parsed_url.path.startswith("/watch")
+            and "v=" in parsed_url.query
+        ):
+            video_id = parsed_url.query.split("v=")[-1].split("&")[0]
+        elif (
+            "youtu.be" in hostname
+            and parsed_url.path.startswith("/")
+            and len(parsed_url.path) > 1  # Ensure there's a video ID
+        ):
+            video_id = parsed_url.path[1:]
+        else:
+            hostname: str = parsed_url.hostname or ""
+            raise UnsupportedPlatformError(hostname)
+
+        def _retrieval_subtitle_of_video_from_youtube() -> Transcription:
+            ytt_api = YouTubeTranscriptApi()
+            transcript_list = ytt_api.list(video_id)
+            transcript = transcript_list.find_transcript(
+                [lang.value for lang in Language]
+                if target_language is None
+                else [target_language.value]
+            )
+            language = Language(transcript.language_code)
+            fetched_transcription = transcript.fetch()
+
+            formatter = WebVTTFormatter()
+            result = formatter.format_transcript(fetched_transcription)
+            return Transcription(
+                content=result, extension=SubtitleExtension.VTT, language=language
+            )
+
+        result = await asyncio_gevent.greenlet_to_future(
+            gevent.spawn(_retrieval_subtitle_of_video_from_youtube)
+        )
+        return result
